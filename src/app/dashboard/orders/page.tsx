4c63a810d525d139c8b177cd2867435cd6921729
IsabelSequera <isaftx1@gmail.com>
2024-06-07 04:23:02 -0400

    correccionUltima

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -33,10 +33,10 @@ const PageOrders = () => {
                 <h1 className="text-white flex flex-col text-2xl font-semibold mb-4">Orders</h1>
             <div className="max-h-screen overflow-y-auto">
             {
-                ordersData?.length > 0? (
+                ordersData?.length > 0 ? (
                     ordersData?.map((order) => {
                         return (
-                            <div className="bg-white p-8 rounded shadow-lg mb-4"> 
+                            <div key={order.id} className="bg-white p-8 rounded shadow-lg mb-4"> 
                                 <div>
                                     <p className="text-gray-700 mb-2">{new Date(order.date).toLocaleDateString()}</p>
                                     <p className="text-gray-700 mb-2">status: {order.status}</p>
@@ -47,7 +47,7 @@ const PageOrders = () => {
                 ) : (
                     <div className="bg-white p-8 rounded shadow-lg">
                         
-                        <p className="text-gray-700 mb-2">You don't have anything added yet!</p>
+                        <p className="text-gray-700 mb-2">You dont have anything added yet!</p>
                         <Link href="/">
                             <label>Buys!</label>
                         </Link>
